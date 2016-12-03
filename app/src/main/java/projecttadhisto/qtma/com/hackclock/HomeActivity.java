56bc179ccb25e7f5e7719613082aabc67a7a679c
NewmanM <m_tanel@live.ca>
2016-12-03 10:15:01 -0500

    Settings activity added, listview and custom adapter

--- app/src/main/java/projecttadhisto/qtma/com/hackclock/HomeActivity.java
+++ app/src/main/java/projecttadhisto/qtma/com/hackclock/HomeActivity.java
@@ -1,5 +1,6 @@
 package projecttadhisto.qtma.com.hackclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -26,6 +30,11 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ArrayList<String> alarms = new ArrayList<>();
+        AlarmAdapter adapter = new AlarmAdapter(alarms, this);
+        ListView lvAlarms = (ListView) findViewById(R.id.lvAlarm);
+        lvAlarms.setAdapter(adapter);
     }
 
     @Override
@@ -44,6 +53,8 @@ public class HomeActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent iSettings = new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(iSettings);
             return true;
         }
 
