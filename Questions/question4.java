1a615162292f04f05515617b6ff919410f0bb084
Mitchell-Mulder <mitch.mulder88@gmail.com>
2016-12-03 15:23:16 -0500

    Added folder with questions and answer java files

---
+++ Questions/question4.java
@@ -0,0 +1,9 @@
+public class Question4 {
+  public public static void main(String [] args)
+	{
+    // Make this for loop execute 15 times
+    for (int i = 1; i < ; i++) {
+      System.out.println(i);
+    }
+	}
+}
---
+++ Questions/question5.java
@@ -0,0 +1,12 @@
+public class Question5 {
+  public public static void main(String [] args)
+	{
+    int i = 0;
+    // Make this if-statment always print "false"
+    if (i == 0) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+	}
+}
