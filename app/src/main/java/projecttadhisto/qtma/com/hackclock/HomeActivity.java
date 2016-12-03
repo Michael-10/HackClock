198ee7c81a0a8313112e9da5d14067ff48aafd9c
NewmanM <m_tanel@live.ca>
2016-12-03 17:11:06 -0500

    Added SQLite database. Can delete alarms, add alarms.

--- app/src/main/java/projecttadhisto/qtma/com/hackclock/HomeActivity.java
+++ app/src/main/java/projecttadhisto/qtma/com/hackclock/HomeActivity.java
@@ -1,37 +1,44 @@
 package projecttadhisto.qtma.com.hackclock;
 
 import android.app.AlarmManager;
+import android.app.LoaderManager;
 import android.app.PendingIntent;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     ArrayList<String> alarms;
-    AlarmAdapter adapter;
     ListView lvAlarms;
     PendingIntent pendingIntent;
     int hour;
     int minute;
+
+    private CursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,7 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,25 +59,34 @@ public class HomeActivity extends AppCompatActivity {
 
         // Create arraylist to store alarms
         alarms = new ArrayList<>();
-        // Need adapter to display alarms
-        adapter = new AlarmAdapter(alarms, this);
-        lvAlarms = (ListView) findViewById(R.id.lvAlarm);
-        // links the adapter to the listview on HomeActivity
-        lvAlarms.setAdapter(adapter);
-        
+
+        // List of columns you want to use when displaying data in layout
+        String[] from = { SQLiteDB.ALARM_TIME };
+        int[] to = {android.R.id.text1};
+
+        // Have to use CursorAdapter with SQLite database
+        // Displays simple text view
+        cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, from, to, 0);
+
+        lvAlarms = (ListView) findViewById(android.R.id.list);
+        lvAlarms.setAdapter(cursorAdapter);
+
+        getLoaderManager().initLoader(0, null, this);
+
+        // Delete list items from database
         lvAlarms.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-                                           int pos, long id) {
+                                           int pos, final long id) {
 
                 final AlertDialog.Builder b = new AlertDialog.Builder(HomeActivity.this);
                 b.setIcon(android.R.drawable.ic_dialog_alert);
                 b.setMessage("Delete?");
                 b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int whichButton) {
-                        TextView tvAlarm = (TextView) findViewById(R.id.list_item_alarm);
-                        alarms.remove(tvAlarm.getText().toString());
-                        adapter.notifyDataSetChanged();
+                        Uri uri = Uri.parse(AlarmsProvider.CONTENT_URI + "/" + id);   // returns primary key value
+                        getContentResolver().delete(uri, SQLiteDB.ALARM_ID + "=" + id, null);
+                        restartLoader();
                     }
                 });
                 b.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -88,9 +104,20 @@ public class HomeActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                
+
             }
         });
+
+        insertAlarm("8:30");
+
+    } // end onCreate method
+
+    // Inserts alarm
+    private void insertAlarm(String time) {
+        ContentValues values = new ContentValues();
+        values.put(SQLiteDB.ALARM_TIME, time);
+        Uri alarmUri = getContentResolver().insert(AlarmsProvider.CONTENT_URI, values);
+        Log.d("HomeActivity", "Inserted Note: " + alarmUri.getLastPathSegment());
     }
 
     public void alarmStart() {
@@ -115,8 +142,8 @@ public class HomeActivity extends AppCompatActivity {
                 String val = data.getStringExtra("setAlarm");
                 hour = data.getIntExtra("hour", -1);
                 minute = data.getIntExtra("minute", -1);
-                alarms.add(val);
-                adapter.notifyDataSetChanged();
+                insertAlarm(hour + ":" + minute);
+                restartLoader();
                 Toast.makeText(this, "Alarm set for " + val, Toast.LENGTH_SHORT).show();
             }
         }
@@ -141,9 +168,45 @@ public class HomeActivity extends AppCompatActivity {
             Intent iSettings = new Intent(HomeActivity.this, SettingsActivity.class);
             startActivity(iSettings);
             return true;
+        } else if (id == R.id.create_sample_data) {
+            insertSampleData();
+        } else if (id == R.id.delete_sample_data) {
+            deleteSampleData();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteSampleData() {
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+                            //Insert Data management code here
+                            getContentResolver().delete(AlarmsProvider.CONTENT_URI, null, null);
+                            restartLoader();
+                            Toast.makeText(HomeActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+    }
+
+    private void insertSampleData() {
+        insertAlarm("10:00");
+        insertAlarm("12:00");
+        insertAlarm("24:00");
+        restartLoader();
+    }
+
+    private void restartLoader() {
+        getLoaderManager().restartLoader(0, null, this);
     }
 
     public void start() {
@@ -173,5 +236,20 @@ public class HomeActivity extends AppCompatActivity {
         /* Repeating on every 20 minutes interval */
         manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 1000 * 60 * 20, pendingIntent);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, AlarmsProvider.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
     }
 }
