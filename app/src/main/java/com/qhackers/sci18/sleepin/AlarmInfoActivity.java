b431bc22691cb8af8c85852195d1426904e51d49
bitscuit <li_zhao_bin@hotmail.com>
2017-06-27 21:28:34 -0400

    AlarmInfo basic functionality done. Need to handle edit/create id

--- app/src/main/java/com/qhackers/sci18/sleepin/AlarmInfoActivity.java
+++ app/src/main/java/com/qhackers/sci18/sleepin/AlarmInfoActivity.java
@@ -1,11 +1,11 @@
 package com.qhackers.sci18.sleepin;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,13 +14,9 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import com.google.gson.Gson;
-
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.Map;
 
 public class AlarmInfoActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,62 +24,86 @@ public class AlarmInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     /**
      * Writes the alarm info to the database.
      * @param v - The view that the event was triggered from (OK button)
      */
-    @TargetApi(23)
     public void saveAlarm(View v) {
-        // get time picker time
         final TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
         int currentApiVersion = Build.VERSION.SDK_INT;
+        int hour = getTimePickerHour(tp, currentApiVersion);
+        int minute = getTimePickerMinute(tp, currentApiVersion);
+        boolean isVibrate = isVibrate();
+        String alarmName = getAlarmName();
+        Alarm a = new Alarm(hour, minute, true, isVibrate, alarmName);
+        writeAlarmToSharedPrefs(a);
+
+        // debug purposes
+        Log.i("TEST", "Hour is: " + hour + " minute is: " + minute + " isVibrate is: " + isVibrate + " alarm name is: " + alarmName);
+    }
+
+    @TargetApi(23)
+    private int getTimePickerHour(TimePicker tp, int currentApiVersion) {
         int hour;
-        int minute;
-        // get hour from time picker
         if (currentApiVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
             hour = tp.getHour(); // for api 23+
         } else {
-            hour = tp.getCurrentHour().intValue();
+            hour = tp.getCurrentHour();
         }
-        // get minute from time picker
+        return hour;
+    }
+
+    @TargetApi(23)
+    private int getTimePickerMinute(TimePicker tp, int currentApiVersion) {
+        int minute;
         if (currentApiVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
             minute = tp.getMinute();
         } else {
-            minute = tp.getCurrentMinute().intValue();
+            minute = tp.getCurrentMinute();
         }
+        return minute;
+    }
 
-        // get vibrateOnOff
+    private boolean isVibrate() {
         final CheckBox cb = (CheckBox) findViewById(R.id.vibrateOnOff);
-        boolean isVibrate = cb.isChecked();
+        return cb.isChecked();
+    }
 
-        // get alarm name
+    @NonNull
+    private String getAlarmName() {
         final EditText et = (EditText) findViewById(R.id.alarmName);
-        String alarmName = et.getText().toString();
+        return et.getText().toString();
+    }
 
-        Log.i("TEST", "Hour is: " + hour + " minute is: " + minute + " isVibrate is: " + isVibrate + " alarm name is: " + alarmName);
-
-        // write to alarm info database
-        String alarmID = "placeholder"; // TODO: get id from intent info
-        Gson g = new Gson();
-        String s = g.toJson(new Alarm(hour, minute, true, isVibrate, alarmName));
+    private void writeAlarmToSharedPrefs(Alarm a) {
+        String alarmID = getIntentExtra("alarmID");
+        String s = getAlarmObjectAsJson(a);
         SharedPreferences sPrefs = getSharedPreferences("Sleepin", MODE_PRIVATE);
         SharedPreferences.Editor pe = sPrefs.edit();
         pe.putString(alarmID, s);
         pe.apply();
 
-        Set<String> test1 = sPrefs.getStringSet("alarms", new HashSet<String>());
-        for (String string : test1) {
-            Log.i("DB", s);
+        // debug purposes
+        for (Map.Entry<String, ?> e : sPrefs.getAll().entrySet()) {
+            Log.i("DB", e.getKey() + " : " + e.getValue());
         }
+    }
 
+    private String getIntentExtra(String key) {
+        Intent intent = getIntent();
+        return intent.getStringExtra(key);
+    }
+
+    private String getAlarmObjectAsJson(Alarm a) {
+        Gson g = new Gson();
+        return g.toJson(a);
     }
 
     /**
      * Cancels changes made to alarm and returns back to AlarmListActivity
-     * @param v
+     * @param v The view that the event was triggered from (Cancel button)
      */
     public void cancelEditCreate(View v) {
         finish();
